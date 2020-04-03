d3d3384a0547fcbc5a7110c2e1d253e8625f03e5
Garrett Serack <garretts@microsoft.com>
2020-04-03 12:09:45 -0700

    initial

---
+++ SettingsForm.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace PushToTalk
+{
+    partial class SettingsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                m_GlobalHook.MouseDownExt -= mouseDown;
+                m_GlobalHook.KeyDown -= keyDown;
+                m_GlobalHook.MouseUp -= mouseUp;
+                m_GlobalHook.KeyUp -= keyUp;
+
+                m_GlobalHook.Dispose();
+
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.keyText = new System.Windows.Forms.TextBox();
+            this.mouseText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.setKey = new System.Windows.Forms.Button();
+            this.setMouse = new System.Windows.Forms.Button();
+            this.clearKey = new System.Windows.Forms.Button();
+            this.clearMouse = new System.Windows.Forms.Button();
+            this.cancelMouse = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.delay = new System.Windows.Forms.NumericUpDown();
+            this.msec = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.delay)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // keyText
+            // 
+            this.keyText.Location = new System.Drawing.Point(195, 45);
+            this.keyText.Name = "keyText";
+            this.keyText.ReadOnly = true;
+            this.keyText.Size = new System.Drawing.Size(241, 26);
+            this.keyText.TabIndex = 0;
+            // 
+            // mouseText
+            // 
+            this.mouseText.Location = new System.Drawing.Point(195, 85);
+            this.mouseText.Name = "mouseText";
+            this.mouseText.ReadOnly = true;
+            this.mouseText.Size = new System.Drawing.Size(241, 26);
+            this.mouseText.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(177, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Bind to key combination";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(169, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Bind to mouse buttons";
+            // 
+            // setKey
+            // 
+            this.setKey.Location = new System.Drawing.Point(442, 42);
+            this.setKey.Name = "setKey";
+            this.setKey.Size = new System.Drawing.Size(75, 32);
+            this.setKey.TabIndex = 4;
+            this.setKey.Text = "Set";
+            this.setKey.UseVisualStyleBackColor = true;
+            this.setKey.Click += new System.EventHandler(this.setKey_Click);
+            // 
+            // setMouse
+            // 
+            this.setMouse.Location = new System.Drawing.Point(442, 82);
+            this.setMouse.Name = "setMouse";
+            this.setMouse.Size = new System.Drawing.Size(75, 32);
+            this.setMouse.TabIndex = 5;
+            this.setMouse.Text = "Set";
+            this.setMouse.UseVisualStyleBackColor = true;
+            this.setMouse.Click += new System.EventHandler(this.setMouse_Click);
+            // 
+            // clearKey
+            // 
+            this.clearKey.Location = new System.Drawing.Point(523, 42);
+            this.clearKey.Name = "clearKey";
+            this.clearKey.Size = new System.Drawing.Size(75, 32);
+            this.clearKey.TabIndex = 6;
+            this.clearKey.Text = "Clear";
+            this.clearKey.UseVisualStyleBackColor = true;
+            this.clearKey.Click += new System.EventHandler(this.clearKey_Click);
+            // 
+            // clearMouse
+            // 
+            this.clearMouse.Location = new System.Drawing.Point(523, 82);
+            this.clearMouse.Name = "clearMouse";
+            this.clearMouse.Size = new System.Drawing.Size(75, 32);
+            this.clearMouse.TabIndex = 7;
+            this.clearMouse.Text = "Clear";
+            this.clearMouse.UseVisualStyleBackColor = true;
+            this.clearMouse.Click += new System.EventHandler(this.clearMouse_Click);
+            // 
+            // cancelMouse
+            // 
+            this.cancelMouse.AutoSize = true;
+            this.cancelMouse.Location = new System.Drawing.Point(264, 120);
+            this.cancelMouse.Name = "cancelMouse";
+            this.cancelMouse.Size = new System.Drawing.Size(334, 24);
+            this.cancelMouse.TabIndex = 8;
+            this.cancelMouse.Text = "Cancel Mouse Events for selected buttons";
+            this.cancelMouse.UseVisualStyleBackColor = true;
+            this.cancelMouse.CheckedChanged += new System.EventHandler(this.cancelMouse_CheckedChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(164, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Mute delay on release";
+            // 
+            // delay
+            // 
+            this.delay.Increment = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.delay.Location = new System.Drawing.Point(195, 7);
+            this.delay.Maximum = new decimal(new int[] {
+            5000,
+            0,
+            0,
+            0});
+            this.delay.Name = "delay";
+            this.delay.Size = new System.Drawing.Size(97, 26);
+            this.delay.TabIndex = 10;
+            this.delay.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.delay.Value = new decimal(new int[] {
+            250,
+            0,
+            0,
+            0});
+            this.delay.ValueChanged += new System.EventHandler(this.delay_ValueChanged);
+            // 
+            // msec
+            // 
+            this.msec.AutoSize = true;
+            this.msec.Location = new System.Drawing.Point(314, 9);
+            this.msec.Name = "msec";
+            this.msec.Size = new System.Drawing.Size(47, 20);
+            this.msec.TabIndex = 11;
+            this.msec.Text = "msec";
+            // 
+            // SettingsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(621, 174);
+            this.Controls.Add(this.msec);
+            this.Controls.Add(this.delay);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cancelMouse);
+            this.Controls.Add(this.clearMouse);
+            this.Controls.Add(this.clearKey);
+            this.Controls.Add(this.setMouse);
+            this.Controls.Add(this.setKey);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.mouseText);
+            this.Controls.Add(this.keyText);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "SettingsForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "PushToTalk Settings";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.delay)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox keyText;
+        private System.Windows.Forms.TextBox mouseText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button setKey;
+        private System.Windows.Forms.Button setMouse;
+        private System.Windows.Forms.Button clearKey;
+        private System.Windows.Forms.Button clearMouse;
+        private System.Windows.Forms.CheckBox cancelMouse;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown delay;
+        private System.Windows.Forms.Label msec;
+    }
+}
+
---
+++ SettingsForm.cs
@@ -0,0 +1,432 @@
+﻿using Gma.System.MouseKeyHook;
+using Microsoft.Win32;
+using NAudio.CoreAudioApi;
+using NAudio.Wave;
+using PushToTalk.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Design.Serialization;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace PushToTalk
+{
+
+    enum State
+    {
+        None,
+        Active,
+        Query
+    }
+    public partial class SettingsForm : Form, IDisposable
+    {
+        private static string None = "<None>";
+        private bool active;
+        
+        private ManualResetEvent quit = new ManualResetEvent(false);
+        private ManualResetEvent trigger = new ManualResetEvent(false);
+
+        // Icon that displays in the tray area, it changes colours based on the state of the microphone
+        private System.Windows.Forms.NotifyIcon notifier;
+
+        private IKeyboardMouseEvents m_GlobalHook;
+        private State mouse;
+        private State key;
+
+        private HashSet<Keys> keys = new HashSet<Keys>();
+        private HashSet<Keys> currentKeys = new HashSet<Keys>();
+
+        private HashSet<MouseButtons> buttons = new HashSet<MouseButtons>();
+        private HashSet<MouseButtons> currentButtons = new HashSet<MouseButtons>();
+
+        private Task loop;
+        private WaveOut wave = new WaveOut();
+
+        private RegistryKey app = Microsoft.Win32.Registry.CurrentUser.CreateSubKey("Software").CreateSubKey("FearTheCowboy").CreateSubKey("PushToTalk");
+
+        public SettingsForm()
+        {
+            InitializeComponent();
+
+            CreateTrayIcon();
+
+            m_GlobalHook = Hook.GlobalEvents();
+
+            m_GlobalHook.MouseDownExt += mouseDown;
+            m_GlobalHook.KeyDown += keyDown;
+
+            m_GlobalHook.MouseUp += mouseUp;
+            m_GlobalHook.KeyUp += keyUp;
+
+            loop = Task.Run(worker);
+            keyText.Text = None;
+            mouseText.Text = None;
+
+            Task.Delay(1).ContinueWith((p) => Invoke(new Action(load)));
+            
+        }
+
+
+        private void save()
+        {
+            app.SetValue("Key",keyText.Text, RegistryValueKind.String);
+            app.SetValue("Buttons",mouseText.Text, RegistryValueKind.String);
+            app.SetValue("Delay", (int)delay.Value, RegistryValueKind.DWord);
+            app.SetValue("CancelMouseEvents", cancelMouse.Checked ? 1 : 0, RegistryValueKind.DWord);
+        }
+
+        private T[] parse<T>( string value )
+        {
+            if( string.IsNullOrEmpty(value) || value == None)
+            {
+                return Enumerable.Empty<T>().ToArray();
+            }
+
+            return value.Split('+').Select(each => (T)Enum.Parse(typeof(T), each)).ToArray();
+        }
+
+        private void load()
+        {
+            keys.Clear();
+            foreach ( var each in parse<Keys>(app.GetValue("Key")?.ToString() ?? None) ) {
+                keys.Add(each);
+            }
+            keyText.Text =keys.Count == 0 ? None:  keys.Select(each => each.ToString()).Aggregate((c, a) => $"{c}+{a}");
+
+            buttons.Clear();
+            foreach (var each in parse<MouseButtons>(app.GetValue("Buttons")?.ToString() ?? None))
+            {
+                buttons.Add(each);
+            }
+            mouseText.Text = buttons.Count == 0 ? None : buttons.Select(each => each.ToString()).Aggregate((c, a) => $"{c}+{a}");
+
+
+            delay.Value = Convert.ToInt32(app.GetValue("Delay", 250));
+            cancelMouse.Checked = app.GetValue("CancelMouseEvents", 1) == (object)1;
+
+            if( keyText.Text == None && mouseText.Text == None)
+            {
+                Show();
+            } else
+            {
+                Hide();
+                mouse = mouseText.Text == None ? State.None : State.Active;
+                key= keyText.Text == None ? State.None : State.Active;
+            }
+        }
+
+        private IAudioEndpointVolume volumeControl
+        {
+            get
+            {
+                IMMDeviceEnumerator deviceEnumerator = null;
+                IMMDevice device = null;
+                try
+                {
+                    deviceEnumerator = (IMMDeviceEnumerator)(new MMDeviceEnumerator());
+                    deviceEnumerator.GetDefaultAudioEndpoint(EDataFlow.eCapture, ERole.eMultimedia, out device);
+
+                    Guid IID_IAudioEndpointVolume = typeof(IAudioEndpointVolume).GUID;
+                    object o;
+                    device.Activate(ref IID_IAudioEndpointVolume, 0, IntPtr.Zero, out o);
+                    IAudioEndpointVolume masterVol = (IAudioEndpointVolume)o;
+
+                    return masterVol;
+                }
+                finally
+                {
+                    if (device != null) {
+                        Marshal.ReleaseComObject(device);
+                    };
+                    if (deviceEnumerator != null) {
+                        Marshal.ReleaseComObject(deviceEnumerator);
+                    }
+                }
+            }
+        }
+
+        private bool MicrophoneMuted {
+            get {
+                IAudioEndpointVolume masterVol = null;
+                try
+                {
+                    bool isMuted;
+                    masterVol = volumeControl;
+                    if (masterVol == null)
+                    {
+                        return false;
+                    }
+                    
+                    masterVol.GetMute(out isMuted);
+                    return isMuted;
+                }
+                finally
+                {
+                    if (masterVol != null)
+                    {
+                        Marshal.ReleaseComObject(masterVol);
+                    }
+                }
+            }
+        
+            set {
+                IAudioEndpointVolume masterVol = null;
+                try
+                {
+                    masterVol = volumeControl;
+                    if (masterVol == null)
+                    {
+                        return;
+                    }
+                    masterVol.SetMute(value, Guid.Empty);
+                }
+                finally
+                {
+                    if (masterVol != null)
+                        Marshal.ReleaseComObject(masterVol);
+                }
+            }
+        }
+
+        private void CreateTrayIcon()
+        {
+            notifier = new System.Windows.Forms.NotifyIcon();
+            notifier.Icon = Properties.Resources.mic_off;
+            notifier.Visible = true;
+
+            var trayContext = new System.Windows.Forms.ContextMenuStrip();
+
+            var show = new System.Windows.Forms.ToolStripMenuItem("Settings");
+            show.Click += (x, y) =>
+            {
+                this.Show();
+            };
+            trayContext.Items.Add(show);
+
+            var exit = new System.Windows.Forms.ToolStripMenuItem("Exit");
+            exit.Click += (x, y) =>
+            {
+                quit.Set();
+                this.Close();
+            };
+            trayContext.Items.Add(exit);
+
+            notifier.Click += Notifier_Click;
+            notifier.ContextMenuStrip = trayContext;
+        }
+
+        private void Notifier_Click(object sender, EventArgs e)
+        {
+            this.Show();
+        }
+
+        private void worker()
+        {
+            var a = new Action(activate);
+            var d = new Action(deactivate);
+            while (true)
+            {
+                ManualResetEvent.WaitAny(new WaitHandle[] { trigger, quit });
+
+                if(quit.WaitOne(0) )
+                {
+                    return;
+                }
+                trigger.Reset();
+
+                if ((currentKeys.Count > 0 && currentKeys.SetEquals(keys)) || (currentButtons.Count > 0 && currentButtons.SetEquals(buttons)))
+                {
+                   this.Invoke(a);
+                }
+                else
+                {
+                    if (active)
+                    {
+                        Task.Delay((int)delay.Value).ContinueWith(p => { this.Invoke(d); });
+                    }
+                }
+            }
+        }
+        private void PlaySound(UnmanagedMemoryStream sound)
+        {
+            using (var provider = new RawSourceWaveStream(sound, new WaveFormat()))
+            {
+                wave.Init(provider);
+                wave.Play();
+            }
+        }
+
+        private void activate()
+        {
+            if (!active)
+            {
+                PlaySound(Properties.Resources.on);
+                active = true;
+                notifier.Icon = Resources.mic_on;
+                MicrophoneMuted = false;
+            }
+        }
+
+        private void deactivate()
+        {
+            active = false;
+            notifier.Icon = Resources.mic_off;
+            PlaySound(Properties.Resources.off);
+            MicrophoneMuted = true;
+        }
+
+        int i = 0;
+        private void keyDown(object sender, KeyEventArgs e)
+        {
+            switch (key)
+            {
+                case State.Active:
+                    if (keys.Count > 0)
+                    {
+                        currentKeys.Add(e.KeyCode);
+                        trigger.Set();
+
+                        // for single-key hotkey, cancel then keypress (so pause, capslock, scroll-lock, etc work )
+                        if (keys.Count == 0 && currentKeys.SetEquals(keys) )
+                        {
+                            e.Handled = true;
+                        }
+                    }
+                    break;
+
+                case State.Query:
+                    keys.Add(e.KeyCode);
+                    e.Handled = true;
+                    keyText.Text = keys.Select(each => each.ToString()).Aggregate((c,a)=> $"{c}+{a}" );
+                    break;
+            }
+        }
+
+        private void keyUp(object sender, KeyEventArgs e)
+        {
+            switch (key)
+            {
+                case State.Active:
+                    currentKeys.Remove(e.KeyCode);
+                    trigger.Set();
+                    break;
+                case State.Query:
+                    key = State.Active;
+                    save();
+                    break;
+            }
+        }
+
+        private void mouseDown(object sender, MouseEventExtArgs e)
+        {
+            // skip button one and two
+            if( e.Button == MouseButtons.Left || e.Button == MouseButtons.Right   )
+            {
+                return;
+            }
+
+            switch (mouse)
+            {
+                case State.Active:
+                    currentButtons.Add(e.Button);
+                    e.Handled = cancelMouse.Checked && buttons.Contains(e.Button);
+                    trigger.Set();
+                    break;
+
+                case State.Query:
+                    buttons.Add(e.Button);
+                    mouseText.Text = buttons.Select(each => each.ToString()).Aggregate((c, a) => $"{c}+{a}");
+                    break;
+            }
+        }
+
+
+        private void mouseUp(object sender, MouseEventArgs e)
+        {
+            // skip button one and two
+            if (e.Button == MouseButtons.Left || e.Button == MouseButtons.Right)
+            {
+                return;
+            }
+
+            switch (mouse)
+            {
+                case State.Active:
+                    currentButtons.Remove(e.Button);
+                    trigger.Set();
+                    break;
+                case State.Query:
+                    mouse = State.Active;
+                    save();
+                    break;
+            }
+        }
+
+        private void setKey_Click(object sender, EventArgs e)
+        {
+            keyText.Text = "Press and hold a key combination";
+            key = State.Query;
+            keys.Clear();
+            currentKeys.Clear();
+        }
+
+        private void clearKey_Click(object sender, EventArgs e)
+        {
+            keyText.Text = None;
+            key = State.None;
+            keys.Clear();
+            currentKeys.Clear();
+            save();
+        }
+
+        private void setMouse_Click(object sender, EventArgs e)
+        {
+            mouseText.Text = "Press a mouse button combination";
+            mouse = State.Query;
+            buttons.Clear();
+            currentButtons.Clear();
+        }
+
+        private void clearMouse_Click(object sender, EventArgs e)
+        {
+            mouseText.Text = None;
+            mouse = State.None;
+            buttons.Clear();
+            currentButtons.Clear();
+            save();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // quit only when really quitting
+            e.Cancel = !quit.WaitOne(0);
+
+            if( e.Cancel)
+            {
+                // hide the form then
+                this.Hide();
+            }
+
+        }
+
+        private void delay_ValueChanged(object sender, EventArgs e)
+        {
+            save();
+        }
+
+        private void cancelMouse_CheckedChanged(object sender, EventArgs e)
+        {
+            save();
+        }
+    }
+}
